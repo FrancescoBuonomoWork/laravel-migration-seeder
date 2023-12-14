2b5cf47ca833d7152406be0910bf33647ccfbf14
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-12-14 14:44:46 +0100

    add controll city and suppressed train

--- app/Http/Controllers/Folder/PageController.php
+++ app/Http/Controllers/Folder/PageController.php
@@ -14,12 +14,10 @@ class PageController extends Controller
     
     public function index() {
         // SELECT * FROM `trains` WHERE(date(departure_time)= CURRENT_DATE );
-        // $trains = Train::where('departure_time', '>', Carbon::now())->get();
-        // $currentDate = Carbon::now()->toDateString();
-        // $trainDate = Train::('departure_time')->toDateSting();
-        // $trains = Train::where('departure_time', '=', $currentDate)->get();
+        $trains = Train::where('departure_time', '>', Carbon::now())->get();
 
-        $trains = Train::where('departure_time', '>', Carbon::today())->where('departure_time', '<', Carbon::tomorrow())->get(); // 13/12/2023 00:00:00
+        // $trains = Train::where('departure_time', '>', Carbon::today())->where('departure_time', '<', Carbon::tomorrow())->get(); 
+        // 13/12/2023 00:00:00
         return view('index',compact('trains'));
     }
 }
--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -18,16 +18,24 @@ class TrainSeeder extends Seeder
             $new_train = new Train();
 
             $new_train->brand = $faker->word();
+            // la citta di destinazione deve essere sempre diversa da quella di partenza
             $new_train->departure_station = $faker->city();
+            
             $new_train->arrival_station = $faker->city();
+            if($new_train->departure_station === $new_train->arrival_station ){
+                $new_train->arrival_station = $faker->city();
+            };
             $new_train->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
             
-            $new_train->arrival_time = $faker->dateTimeBetween('-1 week', '+1 week');
+            $new_train->arrival_time = $faker->dateTimeBetween($new_train->departure_time, '+1 week');
             $new_train->train_code = $faker->numerify('######');
             $new_train->wagons_number = random_int(1,16);
             $new_train->in_time = $faker->randomElement([false, true]);
             $new_train->suppressed = $faker->randomElement([false, true]);
-
+            // se un treno é stato cancellato non puo essere in orario 
+            if($new_train->suppressed === true ) {
+                $new_train->in_time = false;
+            };
             $new_train->save();
         }
     }
